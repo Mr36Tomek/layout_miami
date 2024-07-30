7c2efad2d7a49060f120d0bf12882ca7fe6a6870
Mr36Tomek <mr36tomek@wp.pl>
2024-07-30 20:17:22 +0200

    make build

---
+++ index.7ec58c2e.js
@@ -0,0 +1,2 @@
+document.querySelector(".form").addEventListener("submit",function(e){e.preventDefault()});
+//# sourceMappingURL=index.7ec58c2e.js.map
--- src/scripts/main.js
+++
@@ -1,7 +0,0 @@
-const form = document.querySelector('.form');
-
-form.addEventListener('submit', formsubmit);
-
-function formsubmit(event) {
-  event.preventDefault();
-}
